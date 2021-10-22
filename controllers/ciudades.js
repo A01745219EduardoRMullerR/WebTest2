600c7f8c492bc1111f4bae79f3ccfc22ba8fe392
Eduardo R. Müller <a01745219@itesm.mx>
2021-10-21 23:44:41 -0600

    Ahi va, creo

--- controllers/ciudades.js
+++ controllers/ciudades.js
@@ -5,8 +5,15 @@ exports.postAgregarCiudad = async (req,res)=>{
     const newCity = new CitySchema(req.body)
     const reqName = newCity.nombre
     newCity._id = new mongoose.Types.ObjectId()
+    CitySchema.deleteOne({"nombre": reqName})
     try{
-        await CitySchema.update({"nombre": reqName}, {$inc: {"interes": 1}})
+        await CitySchema.updateOne({"nombre": reqName}, {$inc: {"interes": 1}})
+    }catch(err){
+        console.log(err)
+        res.send({operacion: "error"})
+    }
+    try{
+        await CitySchema.deleteOne({"nombre": reqName}, {"interes": {$eq: 0}})
     }catch(err){
         console.log(err)
         res.send({operacion: "error"})
@@ -26,6 +33,11 @@ exports.postAgregarCiudad = async (req,res)=>{
 
 exports.getObtenerCiudades=async (req,res)=>{
     const city = await CitySchema.find()
+    console.log(city)
+    res.json(city)
+}
+exports.getObtenerPais=async (req, res)=>{
+    const city = await CitySchema.find({"pais": req.body.pais})
     console.log(city)
     res.json(city)
 }
--- routes/ciudades.js
+++ routes/ciudades.js
@@ -3,6 +3,7 @@ const cityController = require("../controllers/ciudades")
 
 router.post('/agregarCiudad',cityController.postAgregarCiudad)
 router.get('/agregarCiudad',cityController.getObtenerCiudades)
+router.post('/obtenerPais', cityController.getObtenerPais)
 router.post('/actualizarCiudad',cityController.postActualizarCiudades)
 router.post('/borrarCiudad',cityController.postBorrarCiudad)
 
